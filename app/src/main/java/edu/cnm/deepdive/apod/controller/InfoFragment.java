2870fb52f2d35cc41ecece9b70bf5a2fa4a8f386
nick-bennett <nbennett10@cnm.edu>
2025-06-05 15:58:32 -0600

    Started InfoFragment.

---
+++ app/src/main/java/edu/cnm/deepdive/apod/controller/InfoFragment.java
@@ -0,0 +1,40 @@
+package edu.cnm.deepdive.apod.controller;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import edu.cnm.deepdive.apod.viewmodel.ApodViewModel;
+
+public class InfoFragment extends BottomSheetDialogFragment {
+
+  private ApodViewModel viewModel;
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    return super.onCreateView(inflater, container, savedInstanceState);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    viewModel = new ViewModelProvider(requireActivity()).get(ApodViewModel.class);
+    viewModel
+        .getApod()
+        .observe(getViewLifecycleOwner(), (apod) -> {
+          // TODO: 6/5/25 Populate view widgets with data from apod.
+        });
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+  }
+
+}
