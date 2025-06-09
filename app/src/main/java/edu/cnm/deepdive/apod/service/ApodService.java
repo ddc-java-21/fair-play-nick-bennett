0641a27cf33b84a0219f1093afa9f0aacf96225b
nick-bennett <nbennett10@cnm.edu>
2025-06-09 11:09:15 -0600

    Low-level download machinery complete.

--- app/src/main/java/edu/cnm/deepdive/apod/service/ApodService.java
+++ app/src/main/java/edu/cnm/deepdive/apod/service/ApodService.java
@@ -1,16 +1,30 @@
 package edu.cnm.deepdive.apod.service;
 
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images.Media;
+import android.provider.MediaStore.MediaColumns;
 import edu.cnm.deepdive.apod.R;
 import edu.cnm.deepdive.apod.model.Apod;
+import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.CompletableEmitter;
+import io.reactivex.rxjava3.core.CompletableOnSubscribe;
+import io.reactivex.rxjava3.core.CompletableSource;
 import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.core.SingleEmitter;
 import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,6 +35,9 @@ import okhttp3.ResponseBody;
 import retrofit2.Response;
 
 public class ApodService {
+
+  private static final String CONTENT_TYPE_HEADER = "Content-Type";
+  private static final int BUFFER_SIZE = 16_384;
 
   @SuppressLint("StaticFieldLeak")
   private static Context context;
@@ -52,20 +69,50 @@ public class ApodService {
         .map(Arrays::asList);
   }
 
-  public Completable downloadImage(Apod apod) {
-    return
-        Single.create((SingleEmitter<ResponseBody> emitter) -> {
-          Response<ResponseBody> response = proxy.download(apod.getHdurl().toString()).execute();
-          if (response.isSuccessful()) {
-            // TODO: 6/6/25 Save file in gallery.
-
-            emitter.onSuccess(response.body());
-          } else {
-            emitter.onError(new IOException(response.message()));
+  /** @noinspection BlockingMethodInNonBlockingContext*/
+  public Completable downloadImage(String title, URL url) {
+    return Completable.create((CompletableEmitter emitter) -> {
+      Response<ResponseBody> response = proxy.download(url.toString()).execute();
+      if (response.isSuccessful()) {
+        ContentValues values = new ContentValues();
+        values.put(MediaColumns.DISPLAY_NAME, title);
+        values.put(MediaColumns.RELATIVE_PATH,
+            context.getString(R.string.apod_directory_format, Environment.DIRECTORY_PICTURES));
+        values.put(MediaColumns.IS_PENDING, 1);
+        String mimeType = response.headers().get(CONTENT_TYPE_HEADER);
+        if (mimeType != null) {
+          values.put(MediaColumns.MIME_TYPE, mimeType);
+        }
+        ContentResolver resolver = context.getContentResolver();
+        Uri uri = resolver.insert(Media.EXTERNAL_CONTENT_URI, values);
+        if (uri != null) {
+          //noinspection DataFlowIssue
+          try (
+              ResponseBody responseBody = response.body();
+              InputStream input =  responseBody.byteStream();
+              OutputStream output = resolver.openOutputStream(uri);
+          ) {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead;
+            while ((bytesRead = input.read(buffer)) >= 0) {
+              output.write(buffer, 0, bytesRead);
+            }
+            values.clear();
+            values.put(MediaColumns.IS_PENDING, 0);
+            resolver.update(uri, values, null, null);
+          } catch (IOException e) {
+            resolver.delete(uri, null, null);
+            emitter.onError(e);
           }
-        })
-        .subscribeOn(scheduler)
-        .ignoreElement();
+        } else {
+          emitter.onError(new RuntimeException());
+        }
+      } else {
+        emitter.onError(new IOException(response.message()));
+      }
+      emitter.onComplete();
+    })
+        .subscribeOn(scheduler);
   }
 
   public static ApodService getInstance() {
